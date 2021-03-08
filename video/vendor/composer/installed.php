6ee89a6729234bc1e0e4ea4ec8e6548a06d641bf
Morshedul Bari Antor <57103691+morshedul-antor@users.noreply.github.com>
2021-03-09 00:49:49 +0600

    Add files via upload

---
+++ video/vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0+no-version-set',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => '1.0.0+no-version-set',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+    'twilio/sdk' => 
+    array (
+      'pretty_version' => '6.11.0',
+      'version' => '6.11.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '463282c0091ba65ba9e9b249e7288cb59fb5ec77',
+    ),
+  ),
+);
